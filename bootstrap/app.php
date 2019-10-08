c155bb70b8c80d4ff78adfe388911262f631e7ac
Chiemela Chinedum <cchinedum@sbsc.com>
2019-10-08 12:26:20 +0100

    Completes Tymons auth setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,7 +21,13 @@ $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
 );
 
+<<<<<<< HEAD
 $app->withFacades();
+=======
+$app->withFacades(true, [
+    'Tymon\JWTAuth\Facades\JWTAuth' => 'JWTAuth'
+]);
+>>>>>>> Completes Tymons auth setup
 
 $app->withEloquent();
 
@@ -61,9 +67,9 @@ $app->singleton(
 //     App\Http\Middleware\ExampleMiddleware::class
 // ]);
 
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
+$app->routeMiddleware([
+    'auth' => App\Http\Middleware\Authenticate::class,
+]);
 
 /*
 |--------------------------------------------------------------------------
@@ -77,8 +83,9 @@ $app->singleton(
 */
 
 // $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
