725ce444f3b1abdf4263bf1ce7e91380b43f5c13
Chiemela Chinedum <cchinedum@sbsc.com>
2019-10-08 12:27:20 +0100

    Adds the user registration and login feature

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class RegisterController extends Controller
+{
+    /**
+     * Creates and authenticates a new user
+     *
+     * @param Request $request
+     * @return JsonResponse
+     * @throws HttpException
+     * @throws ValidationException
+     */
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        if (!$user)
+            throw new HttpException(500, "User could not be created");
+
+        // generate token for this user
+        return response()->json([
+            'success' => true,
+            'data' => [ 'user' => $user],
+            'token' => $this->getAuthTokenData($user),
+        ], 200);
+    }
+}
